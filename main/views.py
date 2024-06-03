0421c35e8b7b6b852c7e667cb7e1d835ec35220d
yashpatel-py <mailforyash04@gmail.com>
2024-06-03 18:36:22 -0500

    Update in model and modal

--- main/views.py
+++ main/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+@login_required
+def save_task(request):
+    if request.method == 'POST':
+        task_name = request.POST.get('task_name')
+        task_note = request.POST.get('task_note')
+        due_date = request.POST.get('due_date')
+        due_time = request.POST.get('due_time')
+        priority = request.POST.get('priority')
+        tasg = request.POST.get('tags')
+        
+        # Basic validation
+        if not task_name:
+            messages.error(request, "Task name is requirted")
+            return render(request, 'main/home.html')
+        
+        # date and time validation
+        try:
+            if due_date:
+                due_date = datetime.strptime(due_date, '%Y-%m-%d').date()
+            if due_time:
+                due_time = datetime.strptime(due_time, '%H:%M').time()
+        except ValueError as e:
+            messages.error(request, 'Invalid date and time format')
+            return render(request, 'main/home.html')
+        
+        # create task
+        task = Task(
+            user = request.user,
+            title = task_name,
+            description = task_note,
+            due_date = due_date,
+            due_time = due_time,
+            priority = int(priority),
+            tasg = tasg
+        )
+        
+        task.save()
+        messages.success(request, "Task successfully created")
+        return redirect('home')
+    else:
+        return render(request, "main/home.html")
