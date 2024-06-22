a6f410ad52a5fd359dd584180ef1e06e306d9f5f
yashpatel-py <mailforyash04@gmail.com>
2024-06-22 01:09:58 -0500

    User Registration

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'account/register.html', {'form': form})
